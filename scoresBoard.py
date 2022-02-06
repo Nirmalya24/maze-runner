4b2971c600fbdbdcdc018d4ab01f3b2351ae27dd
YvoChien <yvochien@gmail.com>
2022-02-05 19:28:51 -0800

    Added functions
    1. Modified insert_sorted_score function to avoid duplicate player's record by updating with better score
    2. Added get_my_record function to display a certain player's  record

--- scoresBoard.py
+++ scoresBoard.py
@@ -2,39 +2,52 @@
 
 import json
 
+# Add new player name and score pair to the score board
 def new_score(name, time) :
     player = {
         "name" : name,
         "time" : time
     }
-
     updat_record(player)
 
-def updat_record(player, file = "scores.json") :    # Do we want to hard code the file name?
+# Read and re-write the updated record back to scores file
+def updat_record(player, file = "scores.json") :
     with open(file, "r+") as scores :
         record = json.load(scores)
         insert_sorted_score(record, player)
         scores.seek(0)
-        print("test")
-        print(record, scores)
         json.dump(record, scores)
         scores.close
 
+# Insert the new record in ascending (time) order <or> 
+# Update the better score for existing player
 def insert_sorted_score(record, player) :
-    count = 0
-    for ele in record :
-        if ele["time"] < player["time"] :
-            count += 1
-        else :
-            break
-    record.insert(count, player)
 
+    # Insert score for existing player
+    if exisit_player(record, player["name"]) :
+        for ele in record :
+            if ele["name"] == player["name"] and ele["time"] > player["time"] :
+                    ele["time"] = player["time"]
+
+    # Insert score for new player
+    else :
+        count = 0
+        for ele in record :
+            if ele["time"] < player["time"] :
+                count += 1
+            else :
+                break
+        record.insert(count, player)
+
+# Check if player already exist
+def exisit_player(record, name) :
     for ele in record :
-        print(ele["name"] + ": " + str(ele["time"]))
-    
+        if ele["name"] == name :
+            return True
+    return False
 
 # Run newScore("name", score) in server.py to add new record
-new_score("someone else", 3)
+new_score("mazeRunner", 7)
 
 #/////////////////////////////////////////////////////////////////////////////#
 
@@ -50,3 +63,22 @@ def print_top_record(file = "scores.json") :
 
 # Run printTopRecord() in server.py to display top 3 winners
 print_top_record()
+
+#/////////////////////////////////////////////////////////////////////////////#
+
+def get_my_record(name, file = "scores.json") :
+    with open(file, "r") as scores :
+        record = json.load(scores)
+
+        found = False
+        for ele in record :
+            if ele["name"] == name :
+                print(ele["name"] + "'s best score: " + str(ele["time"]) + " seconds.\n")
+                found = True
+                break
+        if not found :
+            print(name + " is not found in record.")
+        scores.close
+
+# Run get_my_record() in server.py to display player's best score
+get_my_record("turtle")
